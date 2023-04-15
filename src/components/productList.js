3565e74e6b538792ffaed5365337b1f823b76083
Matper17 <matiasperalta992@gmail.com>
2023-04-15 12:25:22 -0300

    Prueba REACT JS

---
+++ src/components/productList.js
@@ -0,0 +1,48 @@
+import itemList from '../itemList';
+
+const ProductList = ({
+	allProducts,
+	setAllProducts,
+	countProducts,
+	setCountProducts,
+	total,
+	setTotal,
+}) => {
+	const onAddProduct = product => {
+		if (allProducts.find(itemList => itemList.id === product.id)) {
+			const products = allProducts.map(itemList =>
+				itemList.id === product.id
+					? { ...itemList, quantity: itemList.quantity + 1 }
+					: itemList
+			);
+			setTotal(total + product.price * product.quantity);
+			setCountProducts(countProducts + product.quantity);
+			return setAllProducts([...products]);
+		}
+
+		setTotal(total + product.price * product.quantity);
+		setCountProducts(countProducts + product.quantity);
+		setAllProducts([...allProducts, product]);
+	};
+
+	return (
+		<div className='container-items'>
+			{itemList.map(product => (
+				<div className='item' key={product.id}>
+					<figure>
+						<img src={product.img} alt={product.nameProduct} />
+					</figure>
+					<div className='info-product'>
+						<h2>{product.nameProduct}</h2>
+						<p className='price'>${product.price}</p>
+						<button onClick={() => onAddProduct(product)}>
+							Añadir al carrito
+						</button>
+					</div>
+				</div>
+			))}
+		</div>
+	);
+};
+
+export default ProductList
---
+++ src/itemList.js
@@ -0,0 +1,14 @@
+
+const itemList = [
+    {id: 1, nameProduct: "Laptop", tipo: "Computadoras", desc: "Producto premium", marca: "Dell", peso: "1.2kg", price: 120000, img: "./img/dell.jpg", quantity: 1},
+    {id: 2, nameProduct: "Laptop", tipo: "Computadoras", desc: "Gaming", marca: "Msi", peso: "1kg", price: 800000, img: "./img/msi.jpg", quantity: 1},
+    {id: 3, nameProduct: "Laptop", tipo: "Computadoras", desc: "Oficina", marca: "Hp", peso: "1.2kg", price: 200000, img: "./img/hp.jpg", quantity: 1},
+    {id: 4, nameProduct: "Teclado", tipo: "Accesorios", desc: "Producto premium", marca: "Noga", price: 12000, img: "./img/tecladoNoga.jpg", quantity: 1},
+    {id: 5, nameProduct: "Teclado", tipo: "Accesorios", desc: "Producto premium", marca: "Logitech", price: 10000, img: "./img/tecladoLogitech.jpg", quantity: 1},
+    {id: 6, nameProduct: "Mouse", tipo: "Accesorios", desc: "Producto premium", marca: "Noga", price: 5000, img: "./img/mouseNoga.jpg", quantity: 1},
+    {id: 7, nameProduct: "Auriculares", tipo: "Accesorios", desc: "Producto premium", marca: "Jbl", price: 20000, img: "./img/auriJbl.jpg", quantity: 1},
+    {id: 8, nameProduct: "Auriculares", tipo: "Accesorios", desc: "Producto premium", marca: "Sony", price: 19000, img: "./img/auriSony.jpg", quantity: 1}
+
+];
+
+export default itemList
